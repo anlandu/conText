6005ee057584bd84be8bf1f304ec86f8409f6063
anlandu <adu47249@gmail.com>
2018-02-06 17:02:59 -0500

    Daily Commit 2/6: Successfully downloaded top 100 book texts from Gutenberg. TODO: Fix a few files that were not found

--- scripts/getGut.py
+++ scripts/getGut.py
@@ -1,13 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 r=requests.get("https://www.gutenberg.org/browse/scores/top#books-last30")
 soup=BeautifulSoup(r.text, "html5lib")
 top_100=[]
 for n in range(100):
     top_100.append([i['href'] for i in soup.find_all('li')[n].find_all('a')])
-    with open("{}.txt".format(n), 'w+') as f:
+    with open("{}.txt".format(n), 'w+', encoding='utf-8') as f:
         book=requests.get("https://www.gutenberg.org{}".format(top_100[n][0]))
-        # txturl=[i['href'] for i in book.find_all('td').find_all('text/plain')]
-        print(book.text)
-        f.write(book.text)
+        booksoup=BeautifulSoup(book.text, "html5lib")
+        txturl=booksoup("td",text='Plain Text UTF-8')
+        try:
+            url=txturl[0].a['href'][2:]
+            booktxt=requests.get("http://{}".format(url))
+            f.write(booktxt.text)
+        except:
+            f.write("No text found")
