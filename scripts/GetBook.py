b13dc1fd5099fecf242b5ec76cf0a107d795c9a9
anlandu <adu47249@gmail.com>
2018-02-07 09:07:19 -0500

    Books now have title and author as name, rather than number

---
+++ scripts/GetBook.py
@@ -0,0 +1,22 @@
+import json
+import string
+import requests
+import nltk
+import sys
+
+input=sys.argv[1]
+try:
+    input.index("www.gutenberg.org/files")
+except:
+    print("Invalid URL")
+    sys.exit()
+if input[-4:]!=".txt":
+    print("Invalid URL")
+    sys.exit()
+try:
+    res=requests.get(sys.argv[1])
+    with open("../resources/userInputtedBook.txt", 'w+', encoding='utf-8', errors="ignore") as f:
+        f.write(res.text)
+except:
+    print("Invalid URL")
+    sys.exit()
--- scripts/getGut.py
+++ scripts/getGut.py
@@ -5,14 +5,16 @@ r=requests.get("https://www.gutenberg.org/browse/scores/top#books-last30")
 soup=BeautifulSoup(r.text, "html5lib")
 top_100=[]
 for n in range(100):
-    top_100.append([i['href'] for i in soup.find_all('li')[n].find_all('a')])
-    with open("{}.txt".format(n), 'w+', encoding='utf-8') as f:
-        book=requests.get("https://www.gutenberg.org{}".format(top_100[n][0]))
-        booksoup=BeautifulSoup(book.text, "html5lib")
-        txturl=booksoup("td",text='Plain Text UTF-8')
+    book_tag=soup.find_all('ol')[4].find_all('li')[n].find('a')
+    top_100.append([book_tag.text, book_tag['href']])
+    book_title=top_100[n][0].replace(":","—")
+    book_title=book_title[:book_title.index("(")-1]
+    with open("../resources/{}.txt".format(book_title), 'w+', encoding='utf-8') as f:
+        book_page=requests.get("https://www.gutenberg.org{}".format(top_100[n][1]))
+        book_soup=BeautifulSoup(book_page.text, "html5lib")
         try:
-            url=txturl[0].a['href'][2:]
-            booktxt=requests.get("http://{}".format(url))
-            f.write(booktxt.text)
+            txt_url=book_soup("td",text='Plain Text UTF-8')[0].a['href']
+            book_txt=requests.get("http:{}".format(txt_url))
+            f.write(book_txt.text)
         except:
             f.write("No text found")
