e3ca6918d6e1a92a3ccde3882daaa3ed42fffec1
gsegurapa <32302152+gsegurapa@users.noreply.github.com>
2018-02-13 07:25:26 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,6 +1,6 @@
 """
 Tests the getAvgLength function
 """
-import requests
+import re
 import AverageWordLength
 AverageWordLength.getAvgLength("Also sprach Zarathustra. English by Friedrich Wilhelm Nietzsche")
